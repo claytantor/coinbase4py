030044ba05a44a55d478c3663860ae815a68c2ed
claytantor <claytantor@gmail.com>
2014-09-29 23:21:01 -0700

    added tests framework

---
+++ coinbase4py/test/coinbasev1test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class CoinbaseV1ClientTestCase(unittest.TestCase):
+    def runTest(self):
+        assert 1 == 2/2
+
+if __name__ == '__main__':
+    unittest.main()
--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 
 from distutils.core import setup
 
-
 setup(name='coinbase4py',
       version='0.3.3',
       description='Integration Library for the Coinbase API',
       author='Clay Graham',
       author_email='clayatntor@gmail.com',
       url='https://github.com/claytantor/coinbase4py',
-      packages=['coinbase4py'],
+      packages=['coinbase4py','coinbase4py.test'],
      )
